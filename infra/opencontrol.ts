d4ec2b80e34b53cda6447735272162d7a0417958
Dax Raad <d@ironbay.co>
2025-03-10 16:28:47 -0400

    add readonly policy

--- infra/opencontrol.ts
+++ infra/opencontrol.ts
@@ -5,6 +5,7 @@ import { allSecrets } from "./secret";
 new sst.aws.OpenControl("OpenControl", {
   server: {
     handler: "packages/functions/src/opencontrol.handler",
+    policies: ["arn:aws:iam::aws:policy/ReadOnlyAccess"],
     link: [database, ...allSecrets, api],
     url: true,
   },
