1c790df380c6b0a31d552cd7a94be3c776cba15b
Dax Raad <d@ironbay.co>
2025-03-10 16:08:32 -0400

    remove key

--- infra/opencontrol.ts
+++ infra/opencontrol.ts
@@ -2,18 +2,10 @@ import { api } from "./api";
 import { database } from "./database";
 import { allSecrets } from "./secret";
 
-const key = new random.RandomPassword("OpenControlPassword", {
-  length: 16,
-  special: false,
-});
-
 new sst.aws.OpenControl("OpenControl", {
   server: {
     handler: "packages/functions/src/opencontrol.handler",
     link: [database, ...allSecrets, api],
-    environment: {
-      OPENCONTROL_KEY: key.result,
-    },
     url: true,
   },
 });
