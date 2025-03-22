c849e6c0bf712c2c6c29346274e46f54c256e5b9
adamdottv <2363879+adamdottv@users.noreply.github.com>
2025-03-22 06:23:04 -0500

    feat: waf limit to 2rps per caller

--- infra/api.ts
+++ infra/api.ts
@@ -1,5 +1,5 @@
 import { secret } from "./secret";
-import { domain, shortDomain, zone } from "./dns";
+import { domain, shortDomain } from "./dns";
 import { database } from "./database";
 import { webhook } from "./stripe";
 import { bus } from "./bus";
@@ -74,6 +74,69 @@ const apiFn = new sst.aws.Function("ApiFn", {
   url: true,
 });
 
+const provider = new aws.Provider("UsEast1", { region: "us-east-1" });
+
+const webAcl = new aws.wafv2.WebAcl(
+  "ApiWaf",
+  {
+    scope: "CLOUDFRONT",
+    defaultAction: {
+      allow: {},
+    },
+    visibilityConfig: {
+      cloudwatchMetricsEnabled: true,
+      metricName: "api-rate-limit-metric",
+      sampledRequestsEnabled: true,
+    },
+    rules: [
+      {
+        name: "rate-limit-rule",
+        priority: 1,
+        action: {
+          block: {
+            customResponse: {
+              responseCode: 429,
+              customResponseBodyKey: "rate-limit-response",
+            },
+          },
+        },
+        statement: {
+          rateBasedStatement: {
+            limit: 2 * 60, // 2 rps per authorization header
+            evaluationWindowSec: 60,
+            aggregateKeyType: "CUSTOM_KEYS",
+            customKeys: [
+              {
+                header: {
+                  name: "Authorization",
+                  textTransformations: [{ priority: 0, type: "NONE" }],
+                },
+              },
+            ],
+          },
+        },
+        visibilityConfig: {
+          cloudwatchMetricsEnabled: true,
+          metricName: "rate-limit-rule-metric",
+          sampledRequestsEnabled: true,
+        },
+      },
+    ],
+    customResponseBodies: [
+      {
+        key: "rate-limit-response",
+        content: JSON.stringify({
+          type: "rate_limit",
+          code: "too_many_requests",
+          message: "Rate limit exceeded. Please try again later.",
+        }),
+        contentType: "APPLICATION_JSON",
+      },
+    ],
+  },
+  { provider },
+);
+
 export const api = new sst.aws.Router("Api", {
   routes: {
     "/*": apiFn.url,
@@ -81,6 +144,18 @@ export const api = new sst.aws.Router("Api", {
   domain: {
     name: "api." + domain,
     dns: sst.cloudflare.dns({}),
+  },
+  transform: {
+    cdn(args) {
+      if (!args.transform) {
+        args.transform = {
+          distribution: {},
+        };
+      }
+      args.transform!.distribution = {
+        webAclId: webAcl.arn,
+      };
+    },
   },
 });
 
