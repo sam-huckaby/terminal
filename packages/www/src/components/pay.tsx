57d926aacf98badeb0bcb1a799d9a9351125db9f
adamdottv <2363879+adamdottv@users.noreply.github.com>
2025-04-22 11:27:33 -0500

    fix: web pay form

--- packages/www/src/components/pay.tsx
+++ packages/www/src/components/pay.tsx
@@ -27,7 +27,7 @@ const PayComponent: Component<PayProps> = (props) => {
   const [form, { Form, Field }] = createForm({
     validate: zodForm(
       z.object({
-        email: z.string().email().nonempty('# required'),
+        email: z.string().nonempty('# required').email('invalid email address'),
         name: z.string().nonempty('# required'),
         cardNumber: z.string().nonempty('# required'),
         expMonth: z
@@ -95,152 +95,154 @@ const PayComponent: Component<PayProps> = (props) => {
           <h2 class="text-white">add payment information</h2>
         </Line>
         <Switch>
+          <Match when={form.response.status !== 'success'}>
+            <>
+              <Field name="email">
+                {(field, props) => (
+                  <InputLine
+                    {...props}
+                    autofocus
+                    autocomplete="email"
+                    placeholder="email address"
+                    state={field.error ? 'error' : 'normal'}
+                    message={field.error}
+                  />
+                )}
+              </Field>
+              <Field name="name">
+                {(field, props) => (
+                  <InputLine
+                    {...props}
+                    autofocus={false}
+                    autocomplete="cc-name"
+                    placeholder="name on card"
+                    state={field.error ? 'error' : 'normal'}
+                    message={field.error}
+                  />
+                )}
+              </Field>
+              <Field name="cardNumber">
+                {(field, props) => (
+                  <InputLine
+                    {...props}
+                    autofocus={false}
+                    type="tel"
+                    inputmode="numeric"
+                    pattern="[0-9\s]{13,19}"
+                    autocomplete="cc-number"
+                    maxlength="19"
+                    placeholder="card number"
+                    state={field.error ? 'error' : 'normal'}
+                    message={field.error}
+                  />
+                )}
+              </Field>
+              <Line
+                tabindex="-1"
+                state={
+                  !!getError(form, 'expMonth') || !!getError(form, 'expYear')
+                    ? 'error'
+                    : 'normal'
+                }
+              >
+                <div class="flex items-start justify-start w-full flex-wrap">
+                  <div class="flex w-[197px] items-start">
+                    <Field name="expMonth">
+                      {(_field, props) => (
+                        <Input
+                          {...props}
+                          autofocus={false}
+                          state="normal"
+                          classList={{
+                            'max-w-6 w-6': true,
+                            'animate-shake': !!getError(form, 'expMonth'),
+                          }}
+                          type="tel"
+                          inputmode="numeric"
+                          pattern="[0-9]{1,2}"
+                          autocomplete="cc-exp-month"
+                          maxlength="2"
+                          placeholder="mm"
+                        />
+                      )}
+                    </Field>
+                    <span class="mx-2">/</span>
+                    <Field name="expYear">
+                      {(_field, props) => (
+                        <Input
+                          {...props}
+                          autofocus={false}
+                          state="normal"
+                          classList={{
+                            'animate-shake': !!getError(form, 'expMonth'),
+                          }}
+                          type="tel"
+                          inputmode="numeric"
+                          pattern="[0-9]{2,4}"
+                          autocomplete="cc-exp-year"
+                          maxlength="4"
+                          placeholder="yy"
+                        />
+                      )}
+                    </Field>
+                  </div>
+                  <Show
+                    when={
+                      !!getError(form, 'expMonth') || !!getError(form, 'expYear')
+                    }
+                  >
+                    <span class="text-gray-10">
+                      {getError(form, 'expMonth') || getError(form, 'expYear')}
+                    </span>
+                  </Show>
+                </div>
+              </Line>
+              <Field name="cvc">
+                {(field, props) => (
+                  <InputLine
+                    {...props}
+                    autofocus={false}
+                    type="tel"
+                    inputmode="numeric"
+                    pattern="[0-9]{3,4}"
+                    autocomplete="cc-csc"
+                    maxlength="4"
+                    placeholder="cvc"
+                    state={field.error ? 'error' : 'normal'}
+                    message={field.error}
+                  />
+                )}
+              </Field>
+              <Line>
+                <Button
+                  type="submit"
+                  class="bg-orange hover:enabled:bg-orange/80 text-white"
+                >
+                  add payment information
+                </Button>
+              </Line>
+              <Line />
+              <Show when={form.response.status === 'error'}>
+                <Line state="error" class="lowercase">
+                  {form.response.message}
+                </Line>
+              </Show>
+              <Show when={form.response.status !== 'error'}>
+                <Line state="warning">processed securely by stripe</Line>
+              </Show>
+              <Show when={form.submitting}>
+                <Line state="busy">
+                  <span class="mr-2">sending information to stripe</span>
+                  <Spinner />
+                </Line>
+              </Show>
+            </>
+          </Match>
           <Match when={form.response.status === 'success'}>
             <Line state="success">payment information updated</Line>
             <Line />
             <Line class="text-white">complete purchase</Line>
             <Line state="warning">confirm payment in your terminal</Line>
-          </Match>
-          <Match when={!form.submitting || form.response.status === 'error'}>
-            <Field name="email">
-              {(field, props) => (
-                <InputLine
-                  {...props}
-                  autofocus
-                  autocomplete="email"
-                  placeholder="email address"
-                  state={field.error ? 'error' : 'normal'}
-                  message={field.error}
-                />
-              )}
-            </Field>
-            <Field name="name">
-              {(field, props) => (
-                <InputLine
-                  {...props}
-                  autofocus
-                  autocomplete="cc-name"
-                  placeholder="name on card"
-                  state={field.error ? 'error' : 'normal'}
-                  message={field.error}
-                />
-              )}
-            </Field>
-            <Field name="cardNumber">
-              {(field, props) => (
-                <InputLine
-                  {...props}
-                  autofocus={false}
-                  type="tel"
-                  inputmode="numeric"
-                  pattern="[0-9\s]{13,19}"
-                  autocomplete="cc-number"
-                  maxlength="19"
-                  placeholder="card number"
-                  state={field.error ? 'error' : 'normal'}
-                  message={field.error}
-                />
-              )}
-            </Field>
-            <Line
-              tabindex="-1"
-              state={
-                !!getError(form, 'expMonth') || !!getError(form, 'expYear')
-                  ? 'error'
-                  : 'normal'
-              }
-            >
-              <div class="flex items-start justify-start w-full flex-wrap">
-                <div class="flex w-[197px] items-start">
-                  <Field name="expMonth">
-                    {(_field, props) => (
-                      <Input
-                        {...props}
-                        autofocus={false}
-                        state="normal"
-                        classList={{
-                          'max-w-6 w-6': true,
-                          'animate-shake': !!getError(form, 'expMonth'),
-                        }}
-                        type="tel"
-                        inputmode="numeric"
-                        pattern="[0-9]{1,2}"
-                        autocomplete="cc-exp-month"
-                        maxlength="2"
-                        placeholder="mm"
-                      />
-                    )}
-                  </Field>
-                  <span class="mx-2">/</span>
-                  <Field name="expYear">
-                    {(_field, props) => (
-                      <Input
-                        {...props}
-                        autofocus={false}
-                        state="normal"
-                        classList={{
-                          'animate-shake': !!getError(form, 'expMonth'),
-                        }}
-                        type="tel"
-                        inputmode="numeric"
-                        pattern="[0-9]{2,4}"
-                        autocomplete="cc-exp-year"
-                        maxlength="4"
-                        placeholder="yy"
-                      />
-                    )}
-                  </Field>
-                </div>
-                <Show
-                  when={
-                    !!getError(form, 'expMonth') || !!getError(form, 'expYear')
-                  }
-                >
-                  <span class="text-gray-10">
-                    {getError(form, 'expMonth') || getError(form, 'expYear')}
-                  </span>
-                </Show>
-              </div>
-            </Line>
-            <Field name="cvc">
-              {(field, props) => (
-                <InputLine
-                  {...props}
-                  autofocus={false}
-                  type="tel"
-                  inputmode="numeric"
-                  pattern="[0-9]{3,4}"
-                  autocomplete="cc-csc"
-                  maxlength="4"
-                  placeholder="cvc"
-                  state={field.error ? 'error' : 'normal'}
-                  message={field.error}
-                />
-              )}
-            </Field>
-            <Line>
-              <Button
-                type="submit"
-                class="bg-orange hover:enabled:bg-orange/80 text-white"
-              >
-                add payment information
-              </Button>
-            </Line>
-            <Show when={form.response.status === 'error'}>
-              <Line state="error" class="lowercase">
-                {form.response.message}
-              </Line>
-            </Show>
-            <Show when={form.response.status !== 'error'}>
-              <Line />
-            </Show>
-            <Line state="warning">processed securely by stripe</Line>
-          </Match>
-          <Match when={form.submitting}>
-            <Line state="busy">
-              <span class="mr-2">sending information to stripe</span>
-              <Spinner />
-            </Line>
           </Match>
         </Switch>
       </Editor>
