3ad3131df3e986a97afc2694c59b2188bd7fa587
adamdottv <2363879+adamdottv@users.noreply.github.com>
2025-04-23 11:38:03 -0500

    chore: cleanup

--- packages/core/migrations/0034_update_existing_subscriptions.sql
+++
@@ -1,5 +0,0 @@
--- Update all existing subscription records with the current price of their product variant
-UPDATE subscription s
-JOIN product_variant pv ON s.product_variant_id = pv.id
-SET s.price = pv.price
-WHERE s.price IS NULL;
